ec47739f7fb729735250b04397cb28a72a45731f
Matilde Wittrup Enevoldsen <matildeene@outlook.com>
2019-11-14 12:41:42 +0000

    - Organised routes - Create Setting Routes

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,21 @@
 */
 Auth::routes();
 
+//Main Sites
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('om-mig', 'AboutController@index')->name('about');
+
+//Article sites
 Route::get('artikel/ny', 'PostController@create')->name('post.new');
+Route::get('artikel/{id}/s-{slug}', 'PostController@show')->name('post.show');
+Route::get('artikel/slet-{id}', 'PostController@delete')->name('post.destroy');
+
+//Category Sites
 Route::get('katogori/vis/{id}', 'CategoryController@view')->name('category.show');
 Route::get('katogori/new', 'CategoryController@index')->name('category.showForm');
 Route::post('katogori/slet', 'CategoryController@destroy')->name('category.delete');
 Route::post('katogori/new', 'CategoryController@store')->name('category.new');
-Route::get('artikel/{id}/s-{slug}', 'PostController@show')->name('post.show');
-Route::get('artikel/slet-{id}', 'PostController@delete')->name('post.destroy');
+
+//Site Settings
+Route::get('indstillinger/rediger', 'SettingsController@edit')->name('settings.edit');
+Route::post('indstillinger/opdater', 'SettingsController@update')->name('settings.update');
